4b6ad2d8bb596caf732139f84f3b250fa5c54447
rdbyrd <38544246+rdbyrd@users.noreply.github.com>
2018-06-20 13:00:34 -0400

    Add files via upload

---
+++ photoNegatives/addSuccess.php
@@ -0,0 +1,11 @@
+<?php
+    require_once 'includes/database.php';
+    require_once 'includes/header.php';
+?>
+
+<h2 class="jumbotron">Negative added.</h2>
+
+<div class='alert alert-success' role='alert'>Your entry is now included in the database.</div><br>
+
+<?php
+include 'includes/footer.php';
