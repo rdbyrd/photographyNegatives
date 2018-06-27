8a60befa37a594a3023da5308bb50a51e01c140c
Ryan Byrd <38544246+rdbyrd@users.noreply.github.com>
2018-06-27 16:02:54 -0400

    Update deleteNegative.php

--- photoNegatives/deleteNegative.php
+++ photoNegatives/deleteNegative.php
@@ -3,22 +3,22 @@
 include('includes/database.php');
 
 $id = $_POST['id'];
-//if( isset( $_POST['id'])) {
-//    $id = $_POST['id']; 
-//} else {
-//    echo "Did not post an id.";
-//}
+if( isset( $_POST['id'])) {
+   $id = $_POST['id']; 
+} else {
+   echo "Did not post an id.";
+}
 var_dump($_POST);
 
-//$sql = "DELETE FROM negatives WHERE id=$id";
-//
-//if (mysqli_query($conn, $sql)) {
-//    mysqli_close($conn);
-//    header('Location: negativeList.php');
-//    exit;
-//} else {
-//    echo " Error deleting item.";
-//}
+$sql = "DELETE FROM negatives WHERE id=$id";
+
+if (mysqli_query($conn, $sql)) {
+   mysqli_close($conn);
+   header('Location: negativeList.php');
+   exit;
+} else {
+   echo " Error deleting item.";
+}
  
 /*
 //$id = $_POST['id'];
